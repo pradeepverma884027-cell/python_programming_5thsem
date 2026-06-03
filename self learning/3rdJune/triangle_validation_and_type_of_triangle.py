5f668b4577bb06c7298b1d6895693a2e54855527
pradeepverma884027-cell <pradeepverma884027@gmail.com>
2026-06-03 16:13:41 +0530

    Update triangle_validation_and_type_of_triangle.py

--- self learning/3rdJune/triangle_validation_and_type_of_triangle.py
+++ self learning/3rdJune/triangle_validation_and_type_of_triangle.py
@@ -1,1 +1,17 @@
+a=float(input("enter side a"))
+if(a<=0):
+    exit("side cannot be negative")
+b= float(input("enter side b"))
+if(b<=0):
+    exit("side cannot be negative")
+c=float(input("enter side c"))
+if(c<=0):
+    exit("side cannot be negative")
 
+if (b+c>a and c+a>b and a+b>c):
+    if (a!=b!=c):
+        print("Scalene Triangle")
+    elif a==b and b==c and c==a  :
+        print("Equilateral Triangle")
+    else:
+        print("Isoscales Triangle")
