63b3308540a4380beba048d12fb1f39c46fcdefb
pradeepverma884027-cell <pradeepverma884027@gmail.com>
2026-06-03 16:22:25 +0530

    Update retangle_area_parameter.py

--- labwork/Arithmetic_3rdJune/retangle_area_parameter.py
+++ labwork/Arithmetic_3rdJune/retangle_area_parameter.py
@@ -1,1 +1,9 @@
-
+l=int(input("enter the length"))
+if (l<0):
+    exit("length cannot be negative")
+b=int(input("enter the breadth"))
+if(b<0):
+    exit("breadth cannot be  negative")
+else:
+    print("area is :", l*b)
+    print("perimeter is:", 2*(l+b))
