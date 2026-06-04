261e0d7f32c5998662c3fb548139d8918d7fbfad
pradeepverma884027-cell <pradeepverma884027@gmail.com>
2026-06-04 15:10:39 +0530

    Update secret_number.py

--- labwork/iteration_4thJune/secret_number.py
+++ labwork/iteration_4thJune/secret_number.py
@@ -1,12 +1,13 @@
 import random
-
+#secret number generator
 secret_number = random.randint(1, 50)
+#no. of attempts
 attempts = 0
 
 while True:
     guess = int(input("Enter your guess: "))
     attempts += 1
-
+    
     if guess > secret_number:
         print("Too High")
     elif guess < secret_number:
