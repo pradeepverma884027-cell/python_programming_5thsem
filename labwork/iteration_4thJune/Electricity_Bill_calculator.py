c2dc0432a3acb78c5a0b04b4eeebffc271ca1262
pradeepverma884027-cell <pradeepverma884027@gmail.com>
2026-06-04 14:52:14 +0530

    Update Electricity_Bill_calculator.py

--- labwork/iteration_4thJune/Electricity_Bill_calculator.py
+++ labwork/iteration_4thJune/Electricity_Bill_calculator.py
@@ -1,13 +1,14 @@
+#program to calculate the electricity bill
 units = int(input("Enter units: "))
-
+#low category
 if units <= 100:
     bill = units * 5
     category = "Low"
-
+#medium category
 elif units <= 200:
     bill = units * 7
     category = "Medium"
-
+#high category
 else:
     bill = units * 10
     category = "High"
