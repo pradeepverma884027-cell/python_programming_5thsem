d6bd3a40643297ce14377feec45ad064669bd7fc
pradeepverma884027-cell <pradeepverma884027@gmail.com>
2026-06-04 14:49:26 +0530

    Update Armstrong_number.py

--- labwork/iteration_4thJune/Armstrong_number.py
+++ labwork/iteration_4thJune/Armstrong_number.py
@@ -1,13 +1,18 @@
+
+#program to check a number is armstrong or not
 number= int(input("Enter the number :"))
 n = number
+#finding number of digits 
 digits = len(str(number))
 sum_of_powers = 0
 
 while number > 0:
+    #extracting digits from the number
     digit = number % 10
     sum_of_powers += digit ** digits
+    #number after last digit is removed
     number = number // 10
-
+#chechikng armstrong condition
 if sum_of_powers == n:
     print("Armstrong Number")
 else:
