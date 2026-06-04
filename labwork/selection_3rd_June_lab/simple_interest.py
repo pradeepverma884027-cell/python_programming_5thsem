13ad81181f0c9b93a20a636393fe1ea7e984b78a
pradeepverma884027-cell <pradeepverma884027@gmail.com>
2026-06-04 23:45:00 +0530

    Create simple_interest.py

---
+++ labwork/selection_3rd_June_lab/simple_interest.py
@@ -0,0 +1,12 @@
+pa=int(input("enter the pricipal amount"))
+if (pa<=0):
+    exit("principal amount cantnot be negative")
+r=float(input("enter the rate in percentage"))
+if(r<=0):
+    exit("rate cannot be negative")
+t=int(input("enter the time in years"))
+if(t<=0):
+    exit("time cannot be negative")
+else:
+    print("Simple interest is: ",(pa*t*r)/100)
+
