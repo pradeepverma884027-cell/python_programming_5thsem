c8ba4977deac03d77240ed486a293d7f0c08ec88
pradeepverma884027-cell <pradeepverma884027@gmail.com>
2026-06-04 11:51:44 +0530

    Update Attendance_counter.py

--- classwork/iteration_4thJune/Attendance_counter.py
+++ classwork/iteration_4thJune/Attendance_counter.py
@@ -5,4 +5,4 @@ while(student_in_class<30):
     print("Student Entered")
     student_in_class+=1
     print("Attendace Count :", student_in_class)
-print("All students entered")
+print("All students are present")
