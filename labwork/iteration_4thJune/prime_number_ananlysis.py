ed9bfc2d98719e33d476a045c054fc334b0c26d7
pradeepverma884027-cell <pradeepverma884027@gmail.com>
2026-06-04 15:01:51 +0530

    Update prime_number_ananlysis.py

--- labwork/iteration_4thJune/prime_number_ananlysis.py
+++ labwork/iteration_4thJune/prime_number_ananlysis.py
@@ -1,16 +1,18 @@
+#program to check if a number is prime or not. if not prime then display factors
 num = int(input("Enter a number: "))
 
 count = 0
-
+#counting number of factors
 for i in range(1, num + 1):
     if num % i == 0:
         count = count + 1
-
+# condition  for prime number
 if count == 2:
     print(num, "is a Prime Number")
+#condition for non-prime
 else:
     print("Factors:", end=" ")
-
+    #displaying factors of non-prime number
     for i in range(1, num + 1):
         if num % i == 0:
             print(i, end=" ")
