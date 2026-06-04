b6b654b224f618ef6e487c49d31fc3e4a79fff91
pradeepverma884027-cell <pradeepverma884027@gmail.com>
2026-06-04 14:34:46 +0530

    Update prime_number_ananlysis.py

--- labwork/iteration_4thJune/prime_number_ananlysis.py
+++ labwork/iteration_4thJune/prime_number_ananlysis.py
@@ -1,1 +1,19 @@
+num = int(input("Enter a number: "))
 
+count = 0
+
+for i in range(1, num + 1):
+    if num % i == 0:
+        count = count + 1
+
+if count == 2:
+    print(num, "is a Prime Number")
+else:
+    print("Factors:", end=" ")
+
+    for i in range(1, num + 1):
+        if num % i == 0:
+            print(i, end=" ")
+
+    print()
+    print(num, "is not a Prime Number")
