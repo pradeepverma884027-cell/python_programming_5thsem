13bd987c010acc2e6217b3f45d15d545ff44a3af
pradeepverma884027-cell <pradeepverma884027@gmail.com>
2026-06-04 15:42:57 +0530

    Update Electricity_Bill_calculator.py

--- labwork/iteration_4thJune/Electricity_Bill_calculator.py
+++ labwork/iteration_4thJune/Electricity_Bill_calculator.py
@@ -1,18 +1,20 @@
-#program to calculate the electricity bill
-units = int(input("Enter units: "))
-#low category
+#Take units consumed from user
+units = int(input("Enter Units Consumed: "))
+
+# Calculate bill using slab rates
 if units <= 100:
     bill = units * 5
-    category = "Low"
-#medium category
-elif units <= 200:
-    bill = units * 7
-    category = "Medium"
-#high category
-else:
-    bill = units * 10
-    category = "High"
+    category = "Low Consumption"
 
-print("Units Consumed =", units)
-print("Total Bill = ₹", bill)
-print("Category =", category)
+elif units <= 200:
+    bill = (100 * 5) + (units - 100) * 7
+    category = "Medium Consumption"
+
+else:
+    bill = (100 * 5) + (100 * 7) + (units - 200) * 10
+    category = "High Consumption"
+
+# Display result
+print("Units Consumed:", units)
+print("Total Bill: ₹", bill)
+print("Category:", category)
