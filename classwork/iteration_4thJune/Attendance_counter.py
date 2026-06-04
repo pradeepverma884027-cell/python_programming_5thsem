0cda9fcb52f8d8874cbc18b556df72d99b1a52c5
pradeepverma884027-cell <pradeepverma884027@gmail.com>
2026-06-04 11:43:20 +0530

    Create Attendance_counter.py

---
+++ classwork/iteration_4thJune/Attendance_counter.py
@@ -0,0 +1,8 @@
+student_in_class=0
+if(student_in_class=0):
+  print("NO student in the class")
+while(student_in_class<30):
+    print("Student Entered")
+    student_in_class+=1
+    print("Attendace Count :", student_in_class)
+print("All students entered")
