752d6601677075f666abbc3c85aea9dfd0e2e852
pradeepverma884027-cell <pradeepverma884027@gmail.com>
2026-06-04 14:56:27 +0530

    Update Strong_number.py

--- labwork/iteration_4thJune/Strong_number.py
+++ labwork/iteration_4thJune/Strong_number.py
@@ -6,15 +6,17 @@ n = num
 factorial_sum = 0
 
 while num > 0:
+    #extracting last digit 
     digit = num % 10
 
     factorial = 1
+    #calculating factorial
     for i in range(1, digit + 1):
         factorial *= i
-
+    #calculating sum of factorial of digit
     factorial_sum += factorial
     num = num // 10
-#checking statement
+#checking strong condition 
 if factorial_sum == n:
     print(n, "is a Strong Number")
 else:
