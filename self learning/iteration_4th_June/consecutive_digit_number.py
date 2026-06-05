127dbba8d4f8ee4409190ccc13a107b585029840
pradeepverma884027-cell <pradeepverma884027@gmail.com>
2026-06-05 09:57:39 +0530

    Update consecutive_digit_number.py

--- self learning/iteration_4th_June/consecutive_digit_number.py
+++ self learning/iteration_4th_June/consecutive_digit_number.py
@@ -1,1 +1,19 @@
+# Accept a number from the user
+num = input("Enter a number: ")
 
+# Assume the number is consecutive
+is_consecutive = True
+
+# Check each digit with the previous digit
+for i in range(1, len(num)):
+    
+    # Current digit should be exactly 1 greater than previous digit
+    if int(num[i]) != int(num[i - 1]) + 1:
+        is_consecutive = False
+        break
+
+# Display result
+if is_consecutive:
+    print("Consecutive Number")
+else:
+    print("Not a Consecutive Number")
